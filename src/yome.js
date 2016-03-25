9ff3f62b3aa9994d486e8b15398d985c0994283e
Simon Denier <simon@sogilis.com>
2016-03-25 17:24:02 +0100

    Add initial state and geometry functions

--- src/yome.js
+++ src/yome.js
@@ -17,3 +17,43 @@ Reloader.startReloading = (files) => {
 }
 
 Reloader.startReloading(['build/yome.js'])
+
+function l(x) {
+  console.log(x)
+  return x
+}
+
+var Yome = Yome || {}
+
+Yome.initialState = () => {
+  return {
+    sides: [1,2,3,4,5,6,7,8].map(() => new Object())
+  }
+}
+
+Yome.state = Yome.state || Yome.initialState()
+
+Yome.sideCount = (state) => state.sides.length
+
+Yome.sliceTheta = (state) => 2 * Math.PI / Yome.sideCount(state)
+
+Yome.rotate = (theta, point) => {
+  const sint = Math.sin(theta), cost = Math.cos(theta)
+  return {
+    x: (point.x * cost) - (point.y * sint),
+    y: (point.x * sint) + (point.y * cost)
+  }
+}
+
+Yome.radialPoint = (radius, theta) => Yome.rotate(theta, {x: 0, y: radius})
+
+var YOME_SIDE_SIZE = 180
+
+Yome.sidePoints = (state) =>
+  state.sides.map((_, i) => Yome.radialPoint(YOME_SIDE_SIZE, i * Yome.sliceTheta(state)))
+
+Yome.pointsToPointsString = (points) =>
+  points.map(p => p.x + ',' + p.y).join(' ')
+
+l(Yome.pointsToPointsString(Yome.sidePoints(Yome.state)))
+
